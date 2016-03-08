467d8ebe193b4083d74bf767bd099de4627a727b
n/a <n/a>
2016-03-08 17:16:20 +0100

    Initial commit

---
+++ Travel Safety/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Travel Safety
+//
+//  Created by Philip Bale on 3/8/16.
+//  Copyright © 2016 Philip Bale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
