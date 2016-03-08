ce5eb52a6beb9cf825aff5cc4e01d7f221a4547c
= <pbale95@gmail.com>
2016-03-09 00:43:09 +0100

    Removing google maps and adding center action button

--- Travel Safety/View Controllers/MapViewController.m
+++ Travel Safety/View Controllers/MapViewController.m
@@ -45,7 +45,6 @@
     NSLog(@"Place address %@", place.formattedAddress);
     NSLog(@"Place attributions %@", place.attributions.string);
     
-    CLLocationCoordinate2D coordinates = CLLocationCoordinate2DMake(40.683445,-7.0);
     GMSCameraUpdate *updatedCamera = [GMSCameraUpdate setTarget:place.coordinate zoom:12];
     [self.mapView animateWithCameraUpdate:updatedCamera];
 }
@@ -94,7 +93,7 @@ didFailAutocompleteWithError:(NSError *)error {
     InfoViewController *infoController = [[InfoViewController alloc] initWithNibName:@"InfoViewController" bundle:nil];
     [self addChildViewController:infoController];
     [infoController setDelegate:self];
-
+    
     
     [infoController.view setFrame:self.modalContainerView.frame];
     [self.view addSubview:infoController.view];
@@ -112,19 +111,42 @@ didFailAutocompleteWithError:(NSError *)error {
     [addFeedbackViewController didMoveToParentViewController:self];
 }
 
+- (IBAction)centerButtonPressed:(id)sender {
+    NSLog(@"Center button pressed");
+    
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Emergency Contact"
+                                                                   message:nil
+                                                            preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *button1 = [UIAlertAction actionWithTitle:@"Call Police" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * action) {
+        //code to run once button is pressed
+    }];
+    UIAlertAction *button2 = [UIAlertAction actionWithTitle:@"Call Ambulance" style:UIAlertActionStyleDestructive handler:^(UIAlertAction *action) {
+        //code to run once button is pressed
+    }];
+    
+    UIAlertAction *button3 = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        //code to run once button is pressed
+    }];
+    
+    [alert addAction:button1];
+    [alert addAction:button2];
+    [alert addAction:button3];
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 -(void)exitRequested:(ModalViewController *)controller
 {
     [controller.view removeFromSuperview];
 }
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
