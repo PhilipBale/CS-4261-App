bc79ac1f0f31ed4260b473a2372fad9f0cc3c458
= <pbale95@gmail.com>
2016-03-29 21:57:16 +0200

    Feedback routing and controller

---
+++ server/app/controllers/api/v1/feedback_controller.rb
@@ -0,0 +1,52 @@
+class Api::V1::FeedbackController < Api::V1::ApiController
+
+  def post_feedback
+    authenticate_request
+    post_feedback_params
+
+    new_feedback = Feedback.create(post_feedback_params)
+    if !new_feedback.save
+      render json: {error: 'Unable to save new feedback', messages: @user.errors.full_messages, status: :precondition_failed},
+             status: :precondition_failed and return
+    end
+
+    render json: {status: :created, message: 'Feedback succesfully posted', feedback: new_feedback}, status: :created
+  end
+
+  def fetch_feedback
+    authenticate_request 
+
+    relevant_feedback = Array.new
+
+    relevant_feedback = Feedback.all
+
+    render json: {status: :ok, message: 'Feedback fetched', feedback: relevant_feedback}, status: :ok
+
+    # return max_id for post table
+  end
+ 
+
+  private
+  def post_feedback_params
+    params.require(:post).permit(:url_key, :user_id_array)
+  end
+
+
+end
+
+#  id                 :integer          not null, primary key
+#  user_id            :integer          not null
+#  image_url          :string           not null
+#  view_count         :integer          default(0)
+#  heart_count        :integer          default(0)
+#  smirk_count        :integer          default(0)
+#  fire_count         :integer          default(0)
+#  created_ip_address :string           default("")
+#  likes              :integer          default(0)
+#  likes_needed       :integer          default(0)
+#  flagged            :boolean          default(FALSE)
+#  approved           :boolean          default(FALSE)
+#  deleted            :boolean          default(FALSE)
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get '/', to: 'api#index'
+      get 'post_feedback', to: 'feedback#post_feedback'
+      get 'fetch_feedback', to: 'feedback#fetch_feedback'
     end
   end
 
